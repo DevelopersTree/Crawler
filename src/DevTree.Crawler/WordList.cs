772d3bf45515f456b83aeb76699e954e387a8806
Muhammad Azeez <muhammad-azeez@outlook.com>
2017-10-15 23:13:08 +0300

    some optimizations to the wordlist command

--- src/DevTree.Crawler/WordList.cs
+++ src/DevTree.Crawler/WordList.cs
@@ -10,59 +10,55 @@ namespace DevTree.Crawler
 {
     class WordList
     {
-        private static char[] _delimiters = {' ','،',',',';','.','؛',':','.','/','\\','\n'};
-        private static Dictionary<string,int> _duplicates;
+        private static char[] _delimiters = { ' ', '،', ',', ';', '.', '؛', ':', '.', '/', '\\', '\n' };
+        private static Dictionary<string, int> _wordList;
 
         public static void wordlist(string[] args)
         {
             string inputdir = ParameterHelper.GetParameter(args, "-inputdir", " Input Directory ");
             string filename = ParameterHelper.GetParameter(args, "-outfile", " Output File Name ");
-            _duplicates = new Dictionary<string, int>();
+            _wordList = new Dictionary<string, int>();
 
             DirectoryInfo dinfo = new DirectoryInfo(inputdir);
             FileInfo[] TextFiles = dinfo.GetFiles("*.txt", SearchOption.AllDirectories);
             string[] tmpStrArray = null;
-            string wordlist = "";
-            
+
             int i = 0;
             Console.WriteLine("---------Creating wordlist---------");
             var characters = Unicode.SoraniNumbers.ToArray();
-            Word tmpWord = new Word();
             foreach (FileInfo file in TextFiles)
             {
                 tmpStrArray = File.ReadAllText(file.FullName).Split(_delimiters);
-                foreach(string element in tmpStrArray)
+                foreach (string element in tmpStrArray)
                 {
-                    
-                    if (!wordlist.Contains(element) && element.IndexOfAny(characters)==-1)
+                    if (element.IndexOfAny(characters) != -1)
+                        continue;
+
+                    if (_wordList.ContainsKey(element))
                     {
-                        wordlist = wordlist + Environment.NewLine + element;
+                        _wordList[element]++;
                     }
                     else
                     {
-                        if (_duplicates.ContainsKey(element))
-                        {
-                            _duplicates[element]++;
-                        }
-                        else
-                        {
-                            _duplicates.Add(element, 1);
-                        }
+                        _wordList.Add(element, 1);
                     }
                 }
+
                 ConsoleUtil.DrawTextProgressBar(i, TextFiles.Length);
                 i++;
-                tmpStrArray = null;
             }
-            string statstics = "";
-            foreach (KeyValuePair<string, int> pair in _duplicates)
-            {
-                statstics = statstics  + pair.Key + "," + pair.Value + Environment.NewLine;
-            }
-            IOHelper.SaveFile(Path.Combine(inputdir, "/STATSTICS.txt"), statstics);
-            Console.WriteLine(Environment.NewLine + "---------wordlist created---------");
+
+            Console.WriteLine(Environment.NewLine + $"---------wordlist created: {_wordList.Count:n0}---------");
             Console.WriteLine("---------Saving Results---------");
-            IOHelper.SaveFile(Path.Combine(inputdir,filename), wordlist);
+
+            var builder = new StringBuilder(_wordList.Count * 5);
+
+            foreach (KeyValuePair<string, int> pair in _wordList)
+            {
+               builder.Append(pair.Key + "," + pair.Value + Environment.NewLine);
+            }
+            
+            IOHelper.SaveFile(Path.Combine(inputdir, filename), builder.ToString());
             Console.WriteLine("---------Done---------");
         }
     }
